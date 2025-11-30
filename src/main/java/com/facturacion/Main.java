3ac8236e4ea2c6f3a864eea849df2ac0e22da110
mafevar <matiasdiarte321@gmai.com>
2025-11-30 11:00:24 -0300

    empezando el proyecto

---
+++ src/main/java/com/facturacion/Main.java
@@ -0,0 +1,9 @@
+package com.facturacion;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Sistema de Facturación iniciado!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+    }
+    
+}
