7dac1b5f9c7b202c55b219ae9f9e3186550cdd39
mafevar <matiasdiarte321@gmai.com>
2025-11-30 11:11:49 -0300

    me frustre borre todo

--- src/main/java/com/facturacion/Main.java
+++
@@ -1,9 +0,0 @@
-package com.facturacion;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("¡Sistema de Facturación iniciado!");
-        System.out.println("Java version: " + System.getProperty("java.version"));
-    }
-    
-}
